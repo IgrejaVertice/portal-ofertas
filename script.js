7cb8d3e5e4d920a4e901e897cbf8fe9209b0dbe2
Raphael Neumann <raphael@rneumann.me>
2023-05-13 01:28:31 -0300

    feat: add inputmask for other values input

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 $('body').on('click', '.vlr_doacao', function(e){
     $(this).find("input[type='radio']").prop('checked', true);
 });
+$(document).ready(function(){
+    // adiciona mascara ao campo outros na seleção de valores
+    $('input[name=doacao_vlr_outro]').mask("#.##0,00", {reverse: true});
+});
